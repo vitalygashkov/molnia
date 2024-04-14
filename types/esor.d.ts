340b15f132af460e99afeed3e03aa1713672a84e
Vitaly Gashkov <vitalygashkov@vk.com>
2024-04-14 21:35:53 +0500

    feat: add connections option, add onChunkData option

--- types/esor.d.ts
+++ types/esor.d.ts
@@ -1,8 +1,13 @@
 export interface DownloadOptions {
   output?: string;
   headers?: Record<string, string>;
+  connections?: number;
+  onChunkData?: (data: Buffer) => Buffer | void;
 }
 
 export function download(url: string, options?: DownloadOptions): Promise<void>;
 
-export function downloadSegments(urls: string[], options?: DownloadOptions): Promise<void>;
+export function downloadSegments(
+  urls: string[],
+  options?: DownloadOptions,
+): Promise<void>;
