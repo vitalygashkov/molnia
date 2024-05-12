696d6ffe5d2dc10d87ef7ba7b104846e741f7d03
Vitaly Gashkov <vitalygashkov@vk.com>
2024-05-12 12:24:23 +0500

    fix: compare chunk content type to handle invalid response

--- esor.js
+++ esor.js
@@ -14,7 +14,7 @@ const download = async (url, options = {}) => {
   const head = await fetchHead(url);
   options.output = parseOutput(head.url, options.output);
   if (head.isProgressive) {
-    await downloadProgressive(head.url, options, head.contentLength);
+    await downloadProgressive(head.url, options, head.contentLength, head.contentType);
   } else {
     await save({ url: head.url, headers: head, output: options.output });
   }
